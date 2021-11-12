7fc2498c3f4371db0cf52984bb844b5643a89085
Elia Brown <brownelijah143@gmail.com>
2021-11-11 20:55:57 -0700

    api routes adjusted

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,56 +1,40 @@
 const fs = require("fs");
 //const path = require("path");
 //const router = require("express");
-const data = require('../db/db.json');
+//const data = require('../db/db.json');
 const router = require('express').Router();
-const notes = require('../db/db.json');
+//const notes = require('../db/db.json');
 const path = require('path');
 
 
 
 router.get('/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf-8', (error, data) => {
-        if (error) {
-            console.log('file error', error);
-            return;
-        } 
-        res.json(JSON.parse(data))
-    });
+    res.sendFile(path.join(__dirname, "../db/db.json"));
 });
 
 router.post("/notes", (req, res) => {
+    let savedNotes = JSON.parse(fs.readFileSync("../db/db.json", "utf-8"));
     let newNote = req.body;
-    notes.push(newNote);
-    res.json(notes);
-    fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(notes))
-})
+
+    savedNotes.push(newNote);
+    fs.writeFileSync("../db/db.json", JSON.stringify(savedNotes));
+    res.json(newNote);
+});
 
 
 router.delete('/notes/:id', (req, res) => {
-    fs.readFile('./db/db.json', (error, data) => {
-        if (error) {
-            throw error;
-        } else {
-            let notes = [];
-            if (data) {
-                notes = JSON.parse(data);
-            };
-            let note = req.body;
-            note.id = req.params.id;
-            notes.splice(note.id, 1);
-            fs.writeFileSync(
-                path.join(__dirname, '../db/db.json'),
-                JSON.stringify(notes, null, 2),
-                (error, data) => {
-                    if (error) {
-                        throw error;
-                    }
-                    return res.json(notes);
-                }
-            );
-        }
-    });
-    res.json(req.params.id);
+    const removeNotes = JSON.parse(fs.readFileSync("../db/db.json", "utf-8"));
+    let x = req.params.id;
+
+    const removeNote = removeNotes.findIndex(item => item.id === x);
+
+    removeNotes.splice(removeNote, 1);
+
+    fs.writeFileSync("../db/db.json", JSON.stringify(removeNotes));
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
+app.listen(PORT, () => {
+    console.log(`App running on ${PORT}`);
+});
 module.exports = router;
