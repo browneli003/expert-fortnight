c04a3479eab64f0429da7d40d5208d0618fa9c62
Elia Brown <brownelijah143@gmail.com>
2021-11-11 20:36:52 -0700

    routes fixed

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,51 +3,28 @@ const fs = require("fs");
 //const router = require("express");
 const data = require('../db/db.json');
 const router = require('express').Router();
-const { notes } = require('../db/db.json');
+const notes = require('../db/db.json');
 const path = require('path');
 
 
 
 router.get('/notes', (req, res) => {
-    fs.readFile('./db/db.json', (error, data) => {
+    fs.readFile('./db/db.json', 'utf-8', (error, data) => {
         if (error) {
-            throw error;
-        } else {
-            let notes = [];
-            if (data) {
-                notes = JSON.parse(data);
-            }
-            return res.json(notes);
-        }
+            console.log('file error', error);
+            return;
+        } 
+        res.json(JSON.parse(data))
     });
 });
 
-router.post('/notes', (req, res) => {
+router.post("/notes", (req, res) => {
+    let newNote = req.body;
+    notes.push(newNote);
+    res.json(notes);
+    fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(notes))
+})
 
-    fs.readFile('./db/db.json', (error, data) => {
-        if (error) {
-            throw error;
-        } else {
-            let notes = [];
-            if (data) {
-                notes = JSON.parse(data);
-            };
-            let note = req.body;
-            note.id = notes.length;
-            notes.push(note);
-            fs.writeFileSync(
-                path.join(__dirname, '../db/db.json'),
-                JSON.stringify(notes, null, 2),
-                (error, data) => {
-                    if (error) {
-                        throw error;
-                    }
-                }
-            );
-            res.json(notes)
-        }
-    });
-});
 
 router.delete('/notes/:id', (req, res) => {
     fs.readFile('./db/db.json', (error, data) => {
