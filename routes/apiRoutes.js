6bf70b078b4a724225caf8123398f1f72d4ce737
Elia Brown <brownelijah143@gmail.com>
2021-11-15 22:53:01 -0700

    okay were good

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,73 @@
-const fs = require("fs");
-//const path = require("path");
-//const router = require("express");
-//const data = require('../db/db.json');
 const router = require('express').Router();
-//const notes = require('../db/db.json');
+const fs = require('fs');
 const path = require('path');
 
-
-// reads notes in db
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, "../db/db.json"));
+    fs.readFile('./db/db.json', (error, data) => {
+        if (error) {
+            throw error;
+        } else {
+            let notes = [];
+            if (data) {
+                notes = JSON.parse(data);
+            }
+            return res.json(notes);
+        }
+    });
 });
 
 router.post('/notes', (req, res) => {
-    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let newNote = req.body;
 
-    savedNotes.push(newNote);
-    fs.writeFileSync("../db/db.json", JSON.stringify(savedNotes));
-    res.json(newNote);
+    fs.readFile('./db/db.json', (error, data) => {
+        if (error) {
+            throw error;
+        } else {
+            let notes = [];
+            if (data) {
+                notes = JSON.parse(data);
+            };
+            let note = req.body;
+            note.id = notes.length;
+            notes.push(note);
+            fs.writeFileSync(
+                path.join(__dirname, '../db/db.json'),
+                JSON.stringify(notes, null, 2),
+                (error, data) => {
+                    if (error) {
+                        throw error;
+                    }
+                }
+            );
+            res.json(notes)
+        }
+    });
+});
+
+router.delete('/notes/:id', (req, res) => {
+    fs.readFile('./db/db.json', (error, data) => {
+        if (error) {
+            throw error;
+        } else {
+            let notes = [];
+            if (data) {
+                notes = JSON.parse(data);
+            };
+            let note = req.body;
+            note.id = req.params.id;
+            notes.splice(note.id, 1);
+            fs.writeFileSync(
+                path.join(__dirname, '../db/db.json'),
+                JSON.stringify(notes, null, 2),
+                (error, data) => {
+                    if (error) {
+                        throw error;
+                    }
+                    return res.json(notes);
+                }
+            );
+        }
+    });
+    res.json(req.params.id);
 });
 
 
--- server.js
+++ server.js
@@ -13,52 +13,10 @@ app.use(express.json());
   
 app.use(express.static('public'));
 
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
     });
 
-    // Returning index.html page
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-
-// Returning notes.html page
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-
-// Returning the json file holding the notes
-app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
-});
-
-// Returns to index.html
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-
-// Adds the new note to the json array
-app.post("/api/notes", (req, res) => {
-    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
-    let newNote = req.body;
-
-    savedNotes.push(newNote);
-    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
-    res.json(newNote);
-});
-
-// Deletes a note by their id in the json array
-app.delete("/api/notes/:id", (req, res) => {
-    const removeNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
-    let x = req.params.id;
-
-    const removeNote = removeNotes.findIndex( item => item.id === x );
-    
-    removeNotes.splice( removeNote, 1 );
-
-    fs.writeFileSync("./db/db.json", JSON.stringify(removeNotes));
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
